f0e20cbb64ee4cb426bac3dc0d06d39557d1b6b7
Mor Siman Tov <mor.goodsign@gmail.com>
2021-10-23 14:26:17 +0300

    first-draft

---
+++ anomaly_detection_util.cpp
@@ -0,0 +1,35 @@
+#include <cmath>
+
+float avg(float* x, int size) {
+    float sum = 0;
+    for (int i = 0; i <= size; i++) {
+        sum += x[i];
+    }
+    return sum * (1 / float(size));
+}
+
+float var(float* x, int size) {
+    float sum = 0, average;
+    for (int i = 0; i <= size; i++) {
+        sum += (x[i] * x[i]);
+    }
+    sum *= (1 / float(size));
+    average = avg(x, size);
+    return sum - (average * average);
+}
+
+float cov(float* x, float* y, int size) {
+    float sum = 0;
+    for (int i = 0; i <= size; i++) {
+        sum += ((x[i] - avg(x, size)) * (y[i] - avg(y, size)));
+    }
+    return sum * (1 / float(size));
+}
+
+float pearson(float* x, float* y, int size) {
+    return cov(x, y, size) / (sqrt(var(x, size)) * sqrt(var(y, size)));
+}
+
+int main() {
+    return 0;
+}
---
+++ cmake-build-debug/CMakeFiles/anomaly_detection_util.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/anomaly_detection_util.dir/anomaly_detection_util.cpp.obj"
+  "anomaly_detection_util.exe"
+  "anomaly_detection_util.exe.manifest"
+  "anomaly_detection_util.pdb"
+  "libanomaly_detection_util.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/anomaly_detection_util.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
