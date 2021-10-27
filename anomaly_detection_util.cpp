d79f717f8942c7cbbee3496cf1a45ab80cc619bf
ShaiFisher1 <fake@fake.com>
2021-10-27 10:14:37 +0300

    changes to the first three functions

--- anomaly_detection_util.cpp
+++ anomaly_detection_util.cpp
@@ -1,35 +1,45 @@
 #include <cmath>
 
-// (Shai) test to see in which branch
-float avg(float* x, int size) {
+// returns the average
+float avg(float *x, int size) {
     float sum = 0;
     for (int i = 0; i < size; i++) {
         sum += x[i];
     }
-    return sum * (1 / float(size));
+    return sum / float(size);
 }
 
-float var(float* x, int size) {
+// returns the variance of X and Y
+float var(float *x, int size) {
     float sum = 0, average;
+    average = avg(x, size); // the u in the equation
     for (int i = 0; i < size; i++) {
         sum += (x[i] * x[i]);
     }
-    sum *= (1 / float(size));
-    average = avg(x, size);
+    sum = sum / float(size); // the average of x^2
     return sum - (average * average);
 }
 
-float cov(float* x, float* y, int size) {
+// returns the covariance of X and Y, the equation: (COV(x,y) = E(XY)-(E(X)*E(y))
+float cov(float *x, float *y, int size) {
     float sum = 0;
-    for (int i = 0; i <= size; i++) {
-        sum += ((x[i] - avg(x, size)) * (y[i] - avg(y, size)));
+    float avgOfX = avg(x, size);
+    float avgOfY = avg(y, size);
+    for (int i = 0; i < size; i++) {
+        sum += x[i] * y[i];
     }
-    return sum * (1 / float(size));
+    sum = sum / (float) size;
+    return sum - (avgOfX * avgOfY);
 }
 
-float pearson(float* x, float* y, int size) {
-    return cov(x, y, size) / (sqrt(var(x, size)) * sqrt(var(y, size)));
+
+// returns the Pearson correlation coefficient of X and Y, the equation: cov(x,y)/sqrt(var(x))*sqrt(var(y))
+float pearson(float *x, float *y, int size) {
+    double standardDeviationX = sqrt(var(x, size));
+    double standardDeviationY = sqrt(var(x, size));
+    return cov(x, y, size) / (standardDeviationX *standardDeviationY);
 }
+
 
 int main() {
     return 0;
