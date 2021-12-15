1272ce4a00cd0f3731a5adb96565ab83c14364aa
ShaiFisher1 <fake@fake.com>
2021-12-15 09:54:32 +0200

    just added the files MAINTRAIN.APP, MINCIRCLE.CPP,MINCIRCLE.H that we received from the assignment

---
+++ cmake-build-debug/CMakeFiles/anomaly_detection_util.h.dir/DependInfo.cmake
@@ -0,0 +1,23 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/cygdrive/c/Users/adifi/CLionProjects/mor-shai-project/Anomaly_detection_util.cpp" "/cygdrive/c/Users/adifi/CLionProjects/mor-shai-project/cmake-build-debug/CMakeFiles/anomaly_detection_util.h.dir/Anomaly_detection_util.cpp.o"
+  "/cygdrive/c/Users/adifi/CLionProjects/mor-shai-project/MainTrain.cpp" "/cygdrive/c/Users/adifi/CLionProjects/mor-shai-project/cmake-build-debug/CMakeFiles/anomaly_detection_util.h.dir/MainTrain.cpp.o"
+  "/cygdrive/c/Users/adifi/CLionProjects/mor-shai-project/SimpleAnomalyDetector.cpp" "/cygdrive/c/Users/adifi/CLionProjects/mor-shai-project/cmake-build-debug/CMakeFiles/anomaly_detection_util.h.dir/SimpleAnomalyDetector.cpp.o"
+  "/cygdrive/c/Users/adifi/CLionProjects/mor-shai-project/timeseries.cpp" "/cygdrive/c/Users/adifi/CLionProjects/mor-shai-project/cmake-build-debug/CMakeFiles/anomaly_detection_util.h.dir/timeseries.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/anomaly_detection_util.h.dir/cmake_clean.cmake
@@ -0,0 +1,14 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/anomaly_detection_util.h.dir/Anomaly_detection_util.cpp.o"
+  "CMakeFiles/anomaly_detection_util.h.dir/MainTrain.cpp.o"
+  "CMakeFiles/anomaly_detection_util.h.dir/SimpleAnomalyDetector.cpp.o"
+  "CMakeFiles/anomaly_detection_util.h.dir/timeseries.cpp.o"
+  "anomaly_detection_util.h.exe"
+  "anomaly_detection_util.h.pdb"
+  "libanomaly_detection_util.h.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/anomaly_detection_util.h.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
