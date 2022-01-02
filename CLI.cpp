fa5072f89d2fba6f463be1b00cef8866e97450b5
Mor Siman Tov <mor.goodsign@gmail.com>
2022-01-02 22:57:19 +0200

    ex5 added commands

---
+++ CLI.cpp
@@ -0,0 +1,31 @@
+#include "CLI.h"
+
+CLI::CLI(DefaultIO *dio) {
+    this->dio = dio;
+    // here we will push to the commands_lst all the option functions
+}
+
+
+void CLI::start() {
+    int optionNumber = -1;
+    bool finishCLI = false;
+    string options = "Welcome to the Anomaly Detection Server.\n"
+                     "Please choose an option:\n"
+                     "1. upload a time series csv file\n"
+                     "2. algorithm settings\n"
+                     "3. detect anomalies\n"
+                     "4. display results\n"
+                     "5. upload anomalies and analyze results\n"
+                     "6. exit\n";
+//    while (!finishCLI) {
+//    }
+}
+
+
+CLI::~CLI() {
+    // deallocatng the allocated memory of the commands vector
+    int lenOfCommands = commands_lst.size();
+    for (int i = 0; i < lenOfCommands; ++i) {
+        delete commands_lst[i];
+    }
+}
---
+++ commands.h
@@ -0,0 +1,131 @@
+
+
+#ifndef COMMANDS_H_
+#define COMMANDS_H_
+
+#include<iostream>
+#include <string.h>
+#include <fstream>
+#include <vector>
+#include "HybridAnomalyDetector.h"
+
+using namespace std;
+
+class DefaultIO{
+public:
+    virtual string read()=0;
+    virtual void write(string text)=0;
+    virtual void write(float f)=0;
+    virtual void read(float* f)=0;
+    virtual ~DefaultIO(){}
+
+    // you may add additional methods here
+};
+
+// you may add here helper classes
+
+struct State {
+    float threshold = 0.9;
+};
+
+// you may edit this class
+class Command{
+protected: // added this
+    DefaultIO* dio;
+public:
+    // the description of the command
+    const string des;
+    Command(DefaultIO* dio, const string des) : dio(dio), des(des){}
+    virtual void execute(State* state)=0;
+    virtual ~Command(){}
+};
+
+// implement here your command classes
+
+class UploadCSVFile: public Command{
+public:
+    UploadCSVFile(DefaultIO* dio) : Command(dio, "upload a time series csv file.\n") {
+    }
+
+    virtual void execute() {
+        // array of strings to save the instructions of the server
+        string instruction[2] = {"Please upload your local train CSV file.\n",
+                      "Please upload your local test CSV file.\n"};
+        // array of strings to save the CSV's file names
+        string CSVfileName[2] = {"anomalyTrain.csv", "anomalyTest.csv"};
+        // upload twice (two files) - first the train file and then the test file
+        for (int i = 0; i < 2; i++) {
+            dio->write(instruction[i]);
+            // to read from the file
+            string fileName = CSVfileName[i];
+            ofstream out(fileName);
+            string data = "";
+            while (out.eof()!= true){
+                out<<data<<endl;
+            }
+            out.close();
+            dio->write("Upload complete.\n");
+        }
+    }
+};
+
+
+
+class AlgorithmSettings: public Command{
+    AlgorithmSettings(DefaultIO* dio) : Command(dio, "algorithm settings.\n") {
+    }
+
+    virtual void execute(State* state) {
+        float newThreshold;
+        // present the option to change the threshold as long as the entered value is not valid
+        while (true) {
+            dio->write("The current correlation threshold is");
+            dio->write(state->threshold);
+            dio->write("\nType a new threshold\n");
+            // let the user to write a new threshold
+            dio->read(&newThreshold);
+            // if the threshold that the user entered is valid, save it and break
+            if (newThreshold <= 1 && newThreshold > 0) {
+                state->threshold = newThreshold;
+                break;
+            }
+            // if the threshold that the user entered is not valid
+            dio->write("Please choose a value between 0 and 1.\n");
+
+        }
+
+    }
+};
+
+
+
+class DetectAnomalies: public Command{
+
+};
+
+
+
+class ResultsDisplay: public Command{
+
+};
+
+
+
+class UploadAnomAndAnalyzeRes: public Command{
+
+};
+
+
+
+class Exit: public Command{
+    Exit(DefaultIO* dio) : Command(dio, "Exit.\n") {
+    }
+
+    virtual void execute(State* state) {
+        cout<<this->des<<endl;
+    }
+};
+
+
+
+#endif /* COMMANDS_H_ */
