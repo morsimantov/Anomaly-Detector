d59f0c376e06aa60d8ccee982df75f95ab8bb218
ShaiFisher1 <74471077+ShaiFisher1@users.noreply.github.com>
2022-01-09 09:39:09 +0200

    changes in commands.h in function UploadAnomAndAnalyzeRes

--- commands.h
+++ commands.h
@@ -34,13 +34,21 @@ public:
 
 // you may add here helper classes
 
+// will save the report with time steps
+struct fullReport {
+    bool truePositive;
+    int startAnomaly;
+    int endAnomaly;
+    string des;
+};
+
 // will save the inner state
 struct InnerState {
     float threshold = 0.9;
     vector<AnomalyReport> anomalyReport;
-    int sizeOfFile;
+    vector<fullReport> fullReports;
+    int testSize;
 };
-
 
 // you may edit this class
 class Command {
@@ -64,7 +72,7 @@ public:
     UploadCSVFile(DefaultIO *dio) : Command(dio, "upload a time series csv file.\n") {
     }
 
-    virtual void execute() {
+    virtual void execute(InnerState *innerState) {
         // array of strings to save the instructions of the server
         string instruction[2] = {"Please upload your local train CSV file.\n",
                                  "Please upload your local test CSV file.\n"};
@@ -125,11 +133,38 @@ public:
         TimeSeries tsTrain("anomalyTrain.csv");
         TimeSeries tsTest("anomalyTest.csv");
         HybridAnomalyDetector ad;
-        innerState->sizeOfFile = tsTest.getSizeOfTableRows();
+        // create a new full report with time steps
+        fullReport fullReport;
+        fullReport.truePositive = false;
+        fullReport.startAnomaly = 0;
+        fullReport.endAnomaly = 0;
+        fullReport.des = "";
+        // update the inner state
+        innerState->testSize = tsTest.getSizeOfTableRows();
+        // set the threshold of the hybrid anomaly detector
         ad.setThreshold(innerState->threshold);
+        // learn normal on the train file
         ad.learnNormal(tsTrain);
+        // detect on the test file
         innerState->anomalyReport = ad.detect(tsTest);
-
+        // go through the reports of the inner state
+        for_each(innerState->anomalyReport.begin(), innerState->anomalyReport.end(),
+                 [&fullReport, innerState](AnomalyReport &anomalyReport) {
+                     // if the anomalies have the same description and at the same time step
+                     if ((anomalyReport.timeStep == fullReport.endAnomaly + 1) &&
+                         (anomalyReport.description == fullReport.des)) {
+                         fullReport.endAnomaly++;
+                         // if not, save the report
+                     } else {
+                         innerState->fullReports.push_back(fullReport);
+                         fullReport.startAnomaly = anomalyReport.timeStep;
+                         fullReport.endAnomaly = fullReport.startAnomaly;
+                         fullReport.des = anomalyReport.description;
+                     }
+                 });
+        // add the full report
+        innerState->fullReports.push_back(fullReport);
+        innerState->fullReports.erase(innerState->fullReports.begin());
         // at the end of the hybrid algorithm
         this->dio->write("anomaly detection complete.\n");
     }
@@ -142,10 +177,11 @@ public:
     }
 
     virtual void execute(InnerState *innerState) {
-        for_each(innerState->anomalyReport.begin(), innerState->anomalyReport.end(),[this](AnomalyReport& anomalyReport){
-            dio->write(anomalyReport.timeStep);
-            dio->write("\t " +anomalyReport.description+"\n ");
-        });
+        for_each(innerState->anomalyReport.begin(), innerState->anomalyReport.end(),
+                 [this](AnomalyReport &anomalyReport) {
+                     dio->write(anomalyReport.timeStep);
+                     dio->write("\t " + anomalyReport.description + "\n ");
+                 });
         dio->write("Done.\n");
     }
 };
@@ -153,45 +189,62 @@ public:
 
 class UploadAnomAndAnalyzeRes : public Command {
 public:
-    UploadAnomAndAnalyzeRes(DefaultIO* dio) : Command(dio, "upload anomalies and analyze results") {
+    UploadAnomAndAnalyzeRes(DefaultIO *dio) : Command(dio, "upload anomalies and analyze results") {
     }
 
-    bool TruePositive (){
-        // todo function
+    bool TruePositive(int startTime, int endTime, InnerState *innerState) {
+        int fullReportsLen = innerState->fullReports.size();
+        for (int i = 0; i < fullReportsLen; i++) {
+            fullReport f = innerState->fullReports[i];
+            if (f.endAnomaly >= startTime && endTime >= f.startAnomaly) {
+                innerState->fullReports[i].truePositive = true;
+                return true;
+            }
+        }
+        return false;
     }
 
-    virtual void execute(InnerState* innerState){
-        dio->write("Please upload your local anomalies file. \n");
+    virtual void execute(InnerState *innerState) {
+        int fullReportsLen = innerState->fullReports.size();
+
+        // initialize true Positive to be false, before detecting if true
+        for (int i = 0; i < fullReportsLen; i++) {
+            innerState->fullReports[i].truePositive = false;
+        }
+
+        dio->write("Please upload your local anomalies file.\n");
         string clientSring = "";
-        float P=0, TP=0, NP=0 N=0, FP=0; //positive and negative
-        float total=0;
-
-        while ((clientSring=dio->read())!="done"){
+        float P = 0, TP = 0, N = 0, FP = 0, total = 0; //positive and negative
+        while ((clientSring = dio->read()) != "done") {
             int clientSringLen = clientSring.length();
-            int timeStep = 0; // todo size_t (?)
-            for(;clientSring[timeStep]!=','; timeStep++);
-            string start = clientSring.substr(0,timeStep);
-            string end = clientSring.substr(timeStep+1,clientSringLen);
+            int timeStep = 0;
+            for (; clientSring[timeStep] != ','; timeStep++);
+            string start = clientSring.substr(0, timeStep);
+            string end = clientSring.substr(timeStep + 1, clientSringLen);
             int startTime = stoi(start);
             int endTime = stoi(end);
-            if (TruePositive(startTime,endTime,innerState)){
-                TP=TP+1;
+            if (TruePositive(startTime, endTime, innerState)) {
+                TP = TP + 1;
             }
-            total=total+endTime-startTime+1;
+            total = total + endTime - startTime + 1;
             P++;
         }
         dio->write("Upload complete.\n");
-        //todo take care of FP's
-        float tpr=((int)(1000.0*TP/P))/1000.0f;
-        float fpr=((int)(1000.0*FP/N))/1000.0f;
+        fullReportsLen = innerState->fullReports.size();
+        for (int i = 0; i < fullReportsLen; i++) {
+            if (innerState->fullReports[i].truePositive == false) {
+                FP = FP + 1;
+            }
+        }
+        N = innerState->testSize - total;
+        float tpr = ((int) (1000.0 * TP / P)) / 1000.0f;
+        float fpr = ((int) (1000.0 * FP / N)) / 1000.0f;
         dio->write("True Positive Rate: ");
         dio->write(tpr);
         dio->write("\nFalse Positive Rate: ");
         dio->write(fpr);
         dio->write("\n");
     }
-
-
 };
 
 
