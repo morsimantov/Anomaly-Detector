2bd0f9702a00d7c15cf69d93b45f4c0f9d729f65
Mor Siman Tov <mor.goodsign@gmail.com>
2022-01-16 22:37:19 +0200

    final changes in server

--- Server.cpp
+++ Server.cpp
@@ -9,62 +9,88 @@
 
 #include "Server.h"
 
-Server::Server(int port)throw (const char*) {
-    this->stopped = false;
-    this->s = socket(AF_INET, SOCK_STREAM, 0);
+string SocketIO::read() {
+    char charToRead = 0;
+    string strToRead;
+    int charS = sizeof(char);
+    char c = '\n';
+    while (charToRead != c) {
+        recv(this->clientID, &charToRead, charS, 0);
+        if (charToRead == c) {
+            break;
+        }
+        strToRead = strToRead + charToRead;
+    }
+    return strToRead;
+}
+
+
+void SocketIO::write(string text) {
+    const char *txt = text.c_str();
+    send(this->clientID, txt, strlen(txt), 0);
+}
+
+
+void SocketIO::write(float f) {
+    ostringstream strStream;
+    strStream << f;
+    string strToRead(strStream.str());
+    write(strToRead);
+}
+
+void SocketIO::read(float *f) {
+    //  will be shown in the string line
+}
+
+
+Server::Server(int port) throw(const char *) {
+    this->tStopped = false;
+    // create a new socket
+    this->sSocket = socket(AF_INET, SOCK_STREAM, 0);
     // if socket failed
-    if(this->s < 0) {
+    if (this->sSocket < 0)
         throw "socket failed";
-    }
     server.sin_family = AF_INET;
+    server.sin_addr.s_addr = INADDR_ANY;
+    server.sin_port = htons(port);
     // if the binding failed
-    if(bind(this->s, (struct sockaddr*)&server, sizeof(server)) < 0) {
+    if (bind(sSocket, (struct sockaddr *) &server, sizeof(server)) < 0)
         throw "bind failure";
-    }
     // if listen failed
-    if(listen(s, 3) < 0) {
+    if (listen(this->sSocket, 3) < 0)
         throw "listen failure";
-    }
 }
 
-void Server::start(ClientHandler& ch)throw(const char*){
+void sigHandler(int signalNum) {
+    cout << "signal handler" << endl;
 }
 
-void Server::stop(){
-    this->stopped = true;
-    t->join(); // do not delete this!
+
+void Server::start(ClientHandler &ch) throw(const char *) {
+    this->t = new thread([&ch, this]() {
+        signal(SIGALRM, sigHandler);
+        // while the thread hasn't stopped
+        while (!this->tStopped) {
+            socklen_t clientSize = sizeof(client);
+            alarm(1);
+            // accept the client
+            int cSocket = accept(sSocket, (struct sockaddr *) &client, &clientSize);
+            // if the fd is valid
+            if (cSocket > 0) {
+                ch.handle(cSocket);
+                close(cSocket);
+            }
+            alarm(0);
+        }
+        // close the socket
+        close(this->sSocket);
+    });
 }
 
-Server::~Server() {
+
+void Server::stop() {
+    this->tStopped = true;
+    this->t->join(); // do not delete this!
 }
 
-// functions that override defaultIO
-
-string socketIO::read() {
-    char letter = 0;
-    string data = "";
-    size_t size = 0;
-    while(letter != '\n') {
-        recv(this->clientId, &letter, sizeof(char), 0);
-        data += letter;
-    }
-    return data;
-}
-
-void socketIO::read(float* number) {
-// todo do we need this? is it working?
-    recv(this->clientId, number, sizeof(*number), 0);
-}
-
-void socketIO::write(float number) {
-    ostringstream s;
-    s << number;
-    string data(s.str());
-    write(data);
-}
-
-void socketIO::write(string text) {
-    const char* data = text.c_str();
-    int lenData = strlen(data);
-    send(this->clientId, data, lenData,  0);
-}
+Server::~Server() = default;
--- Server.h
+++ Server.h
@@ -7,8 +7,8 @@
  *
  */
 
-#ifndef SERVER_H_
-#define SERVER_H_
+#ifndef EX1_SERVER_H
+#define EX1_SERVER_H
 
 #include "CLI.h"
 
@@ -21,53 +21,55 @@
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
-#include<signal.h>
+#include <signal.h>
 #include <sstream>
 
 using namespace std;
 
 // edit your ClientHandler interface here:
-class ClientHandler{
+class ClientHandler {
 public:
-    virtual void handle(int clientID)=0;
+    virtual void handle(int clientID) = 0;
 };
 
+// you can add helper classes
 
-// you can add helper classes here and implement on the cpp file
-class socketIO:public DefaultIO {
-    int clientId;
+class SocketIO : public DefaultIO {
+    int clientID;
 public:
-    socketIO(int clientId) : clientId(clientId) {
-    }
+    explicit SocketIO(int clientID) : clientID(clientID) {}
     virtual string read();
-    virtual void read(float* number);
-    virtual void write(float number);
-    virtual void write(string data);
+    virtual void write(string text);
+    virtual void write(float f);
+    virtual void read(float *f);
 };
 
 // edit your AnomalyDetectionHandler class here
-class AnomalyDetectionHandler:public ClientHandler{
+class AnomalyDetectionHandler : public ClientHandler {
 public:
-    virtual void handle(int clientID){
-
+    virtual void handle(int clientID) {
+        SocketIO sio(clientID);
+        CLI cli(&sio);
+        cli.start();
     }
 };
 
 // implement on Server.cpp
 class Server {
-    thread* t; // the thread to run the start() method in
     // you may add data members
-    volatile bool stopped;
     // a stream socket
-    int s;
-    //sockaddr_in server;
-    //sockaddr_in client;
+    int sSocket;
+    // the thread to run the start() method in
+    thread *t;
+    volatile bool tStopped;
+    sockaddr_in server;
+    sockaddr_in client;
 
 public:
-    Server(int port) throw (const char*);
+    Server(int port) throw(const char *);
     virtual ~Server();
-    void start(ClientHandler& ch)throw(const char*);
+    void start(ClientHandler &ch) throw(const char *);
     void stop();
 };
 
-#endif /* SERVER_H_ */
+#endif //EX1_SERVER_H
